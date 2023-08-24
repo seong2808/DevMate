eefbd4fcf642e583b6170644cd0114dd53b8abdc
Lemon <tjdqls723@gmail.com>
2023-08-24 13:57:15 +0900

    FIX: CORS 관련 추가

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import 'reflect-metadata';
+import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import groupRoutes from './routes/groupsRoutes';
@@ -14,6 +16,7 @@ import { errorHandler } from './middlewares/error.handler';
 passportIndex();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -34,9 +37,9 @@ const initServer = async () => {
       'mongodb+srv://devmate:elice1234@devmate.eknxqd6.mongodb.net/devmate?retryWrites=true&w=majority',
     );
     console.log('DB 연결');
-
-    app.listen(3000, () => {
-      console.log('서버 실행, 포트 : 3000');
+    const PORT = 8000;
+    app.listen(PORT, () => {
+      console.log(`서버 실행, 포트 : ${PORT}`);
     });
   } catch (err) {
     console.error('Mongoose error:', err);
